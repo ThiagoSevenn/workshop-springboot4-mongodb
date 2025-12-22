f0ae4bb76c364391f66e9857ba491a2d70ff1b13
ThiagoSevenn <thiagonevessc@gmail.com>
2025-12-22 08:45:08 -0300

    Atualização de usuário com PUT

--- src/main/java/com/thiagoneves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/thiagoneves/workshopmongo/resources/UserResource.java
@@ -50,4 +50,13 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @RequestMapping(value = "/{id}",method=RequestMethod.PUT) // OU @Post Mapping
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id){
+        User obj = service.fromDTO(objDTO);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 }
