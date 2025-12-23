4cdd8cad77bdc6a6fa24dc95838cbd2d84c70b09
ThiagoSevenn <thiagonevessc@gmail.com>
2025-12-23 09:56:48 -0300

    Obtendo um post por id

---
+++ src/main/java/com/thiagoneves/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.thiagoneves.workshopmongo.services;
+
+import com.thiagoneves.workshopmongo.domain.Post;
+import com.thiagoneves.workshopmongo.domain.User;
+import com.thiagoneves.workshopmongo.repository.PostRepository;
+import com.thiagoneves.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        Optional<Post> post = postRepository.findById(id);
+        System.out.println(post.get().getTitle());
+        return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
