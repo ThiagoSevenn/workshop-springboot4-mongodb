b6b64431f9df28a4c6961468e2f9021234c0b138
ThiagoSevenn <thiagonevessc@gmail.com>
2025-12-23 09:25:07 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/thiagoneves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/thiagoneves/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.thiagoneves.workshopmongo.config;
 
 import com.thiagoneves.workshopmongo.domain.Post;
 import com.thiagoneves.workshopmongo.domain.User;
+import com.thiagoneves.workshopmongo.dto.AuthorDTO;
 import com.thiagoneves.workshopmongo.repository.PostRepository;
 import com.thiagoneves.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -24,6 +26,7 @@ public class Instantiation implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         postRepository.deleteAll();
         userRepository.deleteAll();
@@ -32,10 +35,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, formatter.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, formatter.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, formatter.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, formatter.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
